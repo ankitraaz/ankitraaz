7be39c949188c428f2c1519a01cefabc908e5e46
Ankit Raaz <ankitraaz@192.168.1.7>
2022-04-21 17:18:11 +0530

    first commit

---
+++ lib/data/json.dart
@@ -0,0 +1,70 @@
+var profile =
+    "https://media-exp1.licdn.com/dms/image/C4E03AQFYniSdoQnWzg/profile-displayphoto-shrink_400_400/0/1620799595147?e=1655942400&v=beta&t=9r6KkdhVfVAsX_urMNtn22wPaqGmTF_N3nbi6KvFqFU";
+
+var recentUsers = [
+  {
+    "image":
+        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "fname": "Alexa",
+    "lname": "Bliss",
+    "date": "18/6/21"
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "fname": "Pat",
+    "lname": "Villa",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1548142813-c348350df52b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "fname": "Dana",
+    "lname": "Rose",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "fname": "Alberto",
+    "lname": "Peter",
+    "text": "Amazing",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "fname": "Niana",
+    "lname": "Micky",
+  },
+  {
+    "image":
+        "https://images.unsplash.com/photo-1523913507744-1970fd11e9ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "fname": "James",
+    "lname": "Rodri",
+  }
+];
+
+List transactions = [
+  {
+    "name": "Niana",
+    "image":
+        "https://images.unsplash.com/photo-1564460576398-ef55d99548b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "price": "\$6,000",
+    "type": 0,
+    "date": "17:30 PM"
+  },
+  {
+    "name": "David",
+    "image":
+        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "price": "\$350",
+    "type": 1,
+    "date": "17:30 PM"
+  },
+  {
+    "name": "Jame",
+    "image":
+        "https://images.unsplash.com/photo-1523913507744-1970fd11e9ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzV8fHByb2ZpbGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "price": "\$50",
+    "type": 0,
+    "date": "17:30 PM"
+  },
+];
